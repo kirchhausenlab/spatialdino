03c32dd651327a9d776ce608e06f5b7fe6d68c27
Alex Lavaee <lavaalex3@gmail.com>
2024-03-01 20:34:48 -0500

    added data prep script

---
+++ tools/data/prepare_data.py
@@ -0,0 +1,286 @@
+from collections import defaultdict
+from functools import partial
+import tifffile as tif
+from pathlib import Path
+from argparse import ArgumentParser
+from typing import Any, Dict, List, Optional, Tuple
+import numpy as np
+from loky import get_reusable_executor
+import h5py
+from dataclasses import dataclass
+
+# - **make new data folder**
+#     - **flatten data (avg or max on z)**
+# - **take several tif files and stack them on the time axis**
+# - shape of data (3, t, z, y, x)
+#     - 3 corresponds to number of color channels
+#     - t is each file or frame which has a shape of z,y,x
+#     - mask as necessary and normalize across t axis
+#     - filter by outliers (say 3 outliers away)
+# - normalize movies over frames for each channels
+#     - take all movies part of 488 nm color channel and stack them (normalize through time)
+#         - leads to consistent intensity
+
+
+# data for lattice microscopes is located in: /nfs/datasync4/tklab-llsm/
+# data for adaptive optics is located in: /nfs/datasync4/AO-LLSM/
+
+
+# possible schema
+
+
+"""
+{"data": {"488nm": DATA, "560nm": DATA, "642nm": DATA, ...}, "metadata": {"stack": []}}
+
+How to extract metadata from filename?
+
+1. split filename by "_"
+2. fname = ex01_CamB_ch1_stack0145_560nm_0469852msec_0178557836msecAbs_000x_000y_000z_0000t
+3. camera = fname[1]
+4. stack = int(fname[3])
+
+where: DATA is concatenated along time axis to form an array of shape (t, z, y, x)
+"""
+
+
+@dataclass
+class Experiment:
+    tif_files: List[Path]
+    experiment_name: str
+
+
+class DataExtractor:
+    def __init__(
+        self,
+        max_parent_directories: int,
+        min_tif_files: int,
+        max_search_depth: int,
+        max_workers: int = 4,
+        timeout: Optional[int] = None,
+    ) -> None:
+        self.max_parent_directories = max_parent_directories
+        self.min_tif_files = min_tif_files
+        self.max_search_depth = max_search_depth
+        self.max_workers = max_workers
+        self.timeout = timeout
+
+    def extract_experiments(
+        self, parent_directory: Path, target_directory: str
+    ) -> List[Experiment]:
+        target_directories = DataExtractor.extract_target_directories(
+            parent_directory,
+            target_directory,
+            self.max_parent_directories,
+            self.max_search_depth,
+        )
+
+        parent_directory_components = parent_directory.parts
+        experiments = DataExtractor._extract_experiments(
+            target_directories, self.min_tif_files, parent_directory_components
+        )
+
+        experiments = [
+            Experiment(tif_files, experiment_name)
+            for experiment_name, tif_files in experiments.items()
+        ]
+
+        return experiments
+
+    @staticmethod
+    def _get_experiment_name(
+        experiment_name: Path, parent_directory_components: Tuple[str, ...]
+    ) -> str:
+        return "".join(
+            [
+                path_component
+                for path_component in experiment_name.parts
+                if path_component not in set(parent_directory_components)
+            ]
+        ).replace(".tif", "")
+
+    def __call__(
+        self, parent_directory: Path, target_directory: str, save_path: Path
+    ) -> None:
+        experiments = self.extract_experiments(parent_directory, target_directory)
+        executor = get_reusable_executor(
+            max_workers=self.max_workers, timeout=self.timeout
+        )
+        save_path.mkdir(parents=True, exist_ok=True)
+        save_data = partial(DataExtractor.save_data, save_path=save_path)
+        executor.map(save_data, experiments)
+
+    @staticmethod
+    def save_data(experiment: Experiment, save_path: Path) -> None:
+        data = DataExtractor.extract_data(experiment.tif_files)
+        save_file_path = save_path.joinpath(f"{experiment.experiment_name}.h5")
+        with h5py.File(save_file_path, "w") as f:
+            DataExtractor.save_dict_to_hdf5(data, f)
+
+        print("Successaved data to: ", save_file_path)
+
+    @staticmethod
+    def save_dict_to_hdf5(data: Dict[str, Any], hdf5_group: h5py.Group) -> None:
+        """
+        Recursively saves a nested dictionary to an HDF5 group.
+
+        Parameters
+        ----------
+        data : Dict[str, Any])
+            The dictionary to save.
+        hdf5_group : h5py.Group
+            The HDF5 group to save the data into. This can be the HDF5 file itself or a subgroup within the file.
+        """
+        for key, value in data.items():
+            if isinstance(value, dict):
+                # Create a subgroup for nested dictionaries
+                subgroup = hdf5_group.create_group(key)
+                DataExtractor.save_dict_to_hdf5(value, subgroup)
+            else:
+                # Directly save non-dictionary values
+                hdf5_group[key] = value
+
+    @staticmethod
+    def extract_data(tif_files: List[Path]) -> Dict[str, dict]:
+        data = DataExtractor.create_data_dict()
+        for tif_file in tif_files:
+            file_name = tif_file.name
+            metadata = DataExtractor.extract_tif_file_metadata(file_name)
+            wavelength = metadata["wavelength"]
+            data["data"][wavelength].append(
+                DataExtractor.extract_tif_file_image(tif_file)
+            )
+            data["metadata"][wavelength]["stack"].append(metadata["stack"])
+        for wavelength in data["data"].keys():
+            data["data"][wavelength] = np.vstack(data["data"][wavelength])
+        return data
+
+    @staticmethod
+    def create_data_dict() -> Dict[str, dict]:
+        data = {
+            "data": defaultdict(list),
+            "metadata": defaultdict(lambda: {"stack": []}),
+        }
+        return data
+
+    @staticmethod
+    def extract_tif_file_image(file_name: Path) -> np.ndarray:
+        data = tif.imread(file_name)
+        return data
+
+    @staticmethod
+    def extract_tif_file_metadata(file_name: str) -> Dict[str, list]:
+        metadata = dict()
+        fname = file_name.split("_")
+        stack = int(fname[3][5:])  # e.g. stack0145 -> 145
+        wavelength = fname[4]
+        metadata["stack"] = stack
+        metadata["wavelength"] = wavelength
+        return metadata
+
+    @staticmethod
+    def directory_walk(
+        parent_directory: Path, target_folder: str, max_search_depth: int
+    ) -> List[Path]:
+        if max_search_depth == 0 or parent_directory.is_file():
+            return []
+        elif parent_directory.name == target_folder:
+            return [parent_directory]
+        return [
+            path
+            for child in parent_directory.iterdir()
+            for path in DataExtractor.directory_walk(
+                child, target_folder, max_search_depth - 1
+            )
+        ]
+
+    @staticmethod
+    def extract_target_directories(
+        parent_directory: Path,
+        target_folder: str,
+        max_parent_directories: int,
+        max_search_depth: int,
+    ) -> List[Path]:
+        target_directories = []
+        while max_parent_directories > 0:
+            target_directories.extend(
+                DataExtractor.directory_walk(
+                    next(parent_directory.iterdir()), target_folder, max_search_depth
+                )
+            )
+            max_parent_directories -= 1
+        return target_directories
+
+    @staticmethod
+    def get_tif_files(directory: Path) -> List[Path]:
+        return list(
+            filter(
+                lambda file: file.suffix == ".tif",
+                (file for file in directory.iterdir()),
+            )
+        )
+
+    @staticmethod
+    def _extract_experiments(
+        directories: List[Path],
+        min_tif_files: int,
+        parent_directory_components: Tuple[str, ...],
+    ) -> Dict[str, List[Path]]:
+        experiments = defaultdict(list)
+        for directory in directories:
+            files = DataExtractor.get_tif_files(directory)
+            experiment_name = DataExtractor._get_experiment_name(
+                files[0], parent_directory_components
+            )
+            if len(files) > min_tif_files:
+                experiments[experiment_name].extend(files)
+        return experiments
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Prepare data for lattice microscopy data into .h5 format."
+    )
+    parser.add_argument(
+        "parent_directory", type=Path, help="Parent directory of the data."
+    )
+    parser.add_argument(
+        "--target_directory", type=str, help="Name of the target directory."
+    )
+    parser.add_argument("--save_path", type=Path, help="Path to save the .h5 files.")
+    parser.add_argument(
+        "--max_parent_directories",
+        type=int,
+        default=1,
+        help="Max number of directories to search in parent directory.",
+    )
+    parser.add_argument(
+        "--min_tif_files",
+        type=int,
+        default=50,
+        help="Min number of .tif files to consider a directory as a target directory.",
+    )
+    parser.add_argument(
+        "--max_search_depth",
+        type=int,
+        default=6,
+        help="Max depth to search for target directory.",
+    )
+    parser.add_argument(
+        "--max_workers", type=int, default=4, help="Max number of workers."
+    )
+    parser.add_argument(
+        "--timeout", type=int, default=None, help="Timeout for each worker."
+    )
+    args = parser.parse_args()
+    data_extractor = DataExtractor(
+        args.max_parent_directories,
+        args.min_tif_files,
+        args.max_search_depth,
+        args.max_workers,
+        args.timeout,
+    )
+    data_extractor(
+        args.parent_directory,
+        args.target_directory,
+        args.save_path,
+    )
---
+++ tools/data/prepare_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python prepare_data.py /nfs/datasync4/tklab-llsm \
+--target_directory DS \
+--save_path /nfs/datasync4/alavaee/tklab-llsm \
+--max_parent_directories 1 \
+--min_tif_files 50 \
+--max_search_depth 6 \
+--max_workers 10
