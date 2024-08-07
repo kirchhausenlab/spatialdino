664450052ba07ce875c88d78c905ee0301281eee
Alex Lavaee <lavaalex3@gmail.com>
2024-08-07 14:39:36 -0400

    Added latest changes for data preparation

--- tools/data/save_data.sh
+++ tools/data/save_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python prepare_data.py \
+python save_data.py \
 --parent_directory /nfs/datasync4/tklab-llsm \
 --search_pattern "**/DS" \
 --directories_up 2 \
 --save_path /nfs/datasync4/alavaee/tklab-llsm \
---found_directories_limit 100 \
+--found_directories_limit 200 \
 --min_tif_files_in_folder 20 \
 --max_workers 100
