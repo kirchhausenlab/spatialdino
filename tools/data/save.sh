3962edb7b979ab91e1e29d5e4948f4def7489231
lavaman131 <lavaalex3@gmail.com>
2024-08-13 10:44:29 -0400

    Slightly modified script to take min_tif_files parameter

--- tools/data/save.sh
+++ tools/data/save.sh
@@ -6,6 +6,6 @@ python save.py \
 --x_voxel_size 224 \
 --parent_directory /nfs/datasync4/tklab-llsm \
 --save_path /nfs/scratch/alavaee/data/processed/llsm \
---found_experiments_limit 1 \
+--found_experiments_limit 50 \
 --min_tif_files 20 \
---max_workers 150
+--max_workers 200
