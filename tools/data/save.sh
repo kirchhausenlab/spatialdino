c7b826aba342eafffd630bb3581b443df1e3d570
lavaman131 <lavaalex3@gmail.com>
2024-08-14 14:13:44 -0400

    Added pth support for dataset

--- tools/data/save.sh
+++ tools/data/save.sh
@@ -5,7 +5,7 @@ python save.py \
 --y_voxel_size 224 \
 --x_voxel_size 224 \
 --parent_directory /nfs/datasync4/tklab-llsm \
---save_path /nfs/scratch/alavaee/data/processed/llsm \
+--save_path /nfs/scratch2/alavaee/data/processed/llsm \
 --found_experiments_limit 50 \
 --min_tif_files 20 \
 --max_workers 200
