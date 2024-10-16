1eeaf1d9c005eb1616c5e888dd91241d476102b2
lavaman131 <lavaalex3@gmail.com>
2024-10-16 18:03:12 -0400

    Re-added featup

--- setup.py
+++ setup.py
@@ -34,13 +34,22 @@ setup(
         "hydra-core>=1.3.2",
         "scipy>=1.14.1",
         "scikit-learn>=1.5.2",
+        "ninja",
+        "jupyter",
+        "ipywidgets",
         "featup @ file://"
         + str(WORKING_DIR.joinpath("src", "third_party", "FeatUp"))
         + "#egg=featup",
     ],
     extras_require={
         "xformers": ["xformers", "triton"],
-        "viz3d": ["pyvista>=0.44.1", "vtk_osmesa", "ninja"],
+        "viz3d-cpu": [
+            "pyvista>=0.44.1",
+            "vtk_osmesa",
+            "trame",
+            "trame-vuetify",
+            "trame-vtk",
+        ],
         "dev": ["pytest>=8.3.2", "ruff>=0.6.2"],
     },
     classifiers=[
